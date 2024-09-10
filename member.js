b7b69fdbc8a65635617bb2b36dba8516f3be8b1c
Kalama <97434062+Kmtengo@users.noreply.github.com>
2024-09-10 08:24:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMembers() {
+  return [
+    {
+      name: 'John Doe',
+      skills: ['Javascript', 'React', 'Vue']
+    },
+    {
+      name: 'Jane Doe',
+      skills: ['HTML', 'CSS', 'Javascript']
+    }
+  ]
+}
